13d168273d25d1006b8b0826ecceee9899f37799
septianabdulkarim-cloud <septianabdulkarim@gmail.com>
2025-08-07 12:34:16 +0700

    Update index, style, dan perbaikan backend di app.py

--- app.py
+++ app.py
@@ -3,13 +3,28 @@ from flask_mail import Mail, Message
 from itsdangerous import URLSafeTimedSerializer
 from database.db import get_db_connection, init_db
 from dotenv import load_dotenv
+from flask import send_from_directory
+from markupsafe import escape
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
 import os, uuid, logging
+
+
+
 
 # Load .env
 load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
+
+
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["200 per day", "50 per hour"]
+)
 
 # Logging
 if not os.path.exists("logs"):
@@ -95,9 +110,16 @@ def verify_email(token):
     return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET', 'POST'])
+@limiter.limit("5 per minute", error_message="Terlalu banyak percobaan login. Coba lagi nanti.")
 def login():
     if request.method == 'POST':
-        email = request.form['email']
+        # Escape input untuk hindari XSS
+        email = escape(request.form['email'].strip().lower())
+
+        if not email or '@' not in email:
+            flash("Format email tidak valid!", "danger")
+            return render_template("login.html")
+
         conn = get_db_connection()
         cur = conn.cursor()
         cur.execute("SELECT token, is_verified FROM users WHERE email = ?", (email,))
@@ -109,12 +131,19 @@ def login():
             if verified:
                 session['email'] = email
                 flash("Login berhasil!", "success")
-                return redirect(url_for('client_dashboard', token=token))
+
+                # Cek apakah email adalah admin
+                if email in ADMIN_EMAILS:
+                    return redirect(url_for('admin_dashboard', token=token))
+                else:
+                    return redirect(url_for('client_dashboard', token=token))
             else:
                 flash("Akun belum diverifikasi!", "warning")
         else:
             flash("Email tidak ditemukan!", "danger")
+
     return render_template("login.html")
+
 
 @app.route('/dashboard/<token>')
 def client_dashboard(token):
@@ -128,6 +157,14 @@ def client_dashboard(token):
         return render_template("dashboard.html", email=user[0])
     flash("Anda harus login terlebih dahulu!", "danger")
     return redirect(url_for('login'))
+
+@app.route('/admin')
+def admin():
+    return send_from_directory('admin_build', 'index.html')
+
+@app.route('/admin/<path:path>')
+def admin_static(path):
+    return send_from_directory('admin_build', path)
 
 @app.route('/logout')
 def logout():
